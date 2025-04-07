05cf8d46651dc8dde28434f604637a82eb9632cf
Cosmin-Valentin <cosminv.nicola@gmail.com>
2025-04-07 08:41:09 +0300

    left/right button push effect for level 4-5

--- assets/js/helper/moveShip.js
+++ assets/js/helper/moveShip.js
@@ -8,6 +8,9 @@ export function startMoving(direction) {
   shipImage.style.removeProperty('transition')
   shipImage.style.transition = 'transform 0.3s ease, filter 0.3s ease'
   shipWrapper.classList.add(`move-${direction}`)
+  document
+    .querySelector(`.bottom-dashboard.bottom-${direction}`)
+    .classList.add('pressed')
 
   if (moveInterval) clearInterval(moveInterval)
   moveInterval = setInterval(() => {
@@ -18,6 +21,9 @@ export function startMoving(direction) {
 export function stopMoving() {
   shipImage.style.removeProperty('transition')
   shipWrapper.classList.remove('move-left', 'move-right')
+  document
+    .querySelectorAll('.bottom-dashboard')
+    .forEach((el) => el.classList.remove('pressed'))
 
   clearInterval(moveInterval)
 }
